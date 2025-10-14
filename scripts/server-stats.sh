f066ec17b1afd584a689a5dbfe61cef040aee7d4
n4hu3lj <nahuelgtasa2@gmail.com>
2025-10-14 16:29:53 +0200

    added script

---
+++ scripts/server-stats.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# uso CPU
+echo "--------- || Use of CPU: || ---------"
+mpstat -P ALL
+echo ""
+
+# uso memoria total (free y en uso)
+echo "-------- || Use of Memory(mb): || ---------"
+free -m
+echo ""
+# uso disco (free y en uso)
+echo "---- || DISKS: || ----"
+lsblk
+echo ""
+echo "---- || Disk Usage: || ----"
+df -h
+echo "---- || Total disk space: || ----"
+df -h --total | grep total
+echo ""
+# top 5 procesos
+echo "---- || Top 5 Processes by usage: || ----"
+ps aux k-pcpu | head -6
+echo ""
+
+echo "---- || System info || ----"
+uname -a
+echo ""
+
+echo "---- || Logged users in the server || ----"
+echo ""
+
+echo "---- || You are || ----"
+whoami
